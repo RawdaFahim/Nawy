97689eb6a3bd36cd4a5f0e1b65521b060f1579c7
RawdaFahim <rawdahfahim@gmail.com>
2025-05-10 12:02:01 +0300

    updated server.js

--- node-hello/index.js
+++ node-hello/index.js
@@ -4,11 +4,10 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   console.log(`Received request: ${req.method} ${req.url}`);  // Log entry
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain'); 
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
 
 server.listen(port, () => {
-  console.log(`Server running on http://0.0.0.0:${port}/`);
+  console.log(`Server running on http://localhost:${port}/`);
 });
